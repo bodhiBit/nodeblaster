8e29db52579893ea31f2711147e4a921356efd02
bodhiBit <TGiessingP@gmail.com>
2013-04-07 17:28:11 +0200

    CSS linting.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
-
 /*
  * GET home page.
  */
+/*jslint indent: 2, node: true */
+"use strict";
 
-exports.index = function(req, res){
+exports.index = function (req, res) {
   res.render("index");
 };
+
